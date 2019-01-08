f246a7ec618d058d2ff18a8b19aabfa1755f6d2a
paddyroddy <patrickjamesroddy@gmail.com>
2019-01-08 13:13:45 +0000

    update jekyll_get plugins file

--- _plugins/jekyll_get.rb
+++ _plugins/jekyll_get.rb
@@ -7,8 +7,19 @@ module Jekyll_Get
     safe true
     priority :highest
 
+    def loadFromCache(data_source, name)
+      path = "#{data_source}/#{name}.json"
+      if not File.exist?(path)
+        return
+      end
+      File.open(path,"r") do |file|
+        return JSON.load(file.read())
+      end
+    end
+
     def generate(site)
       config = site.config['jekyll_get']
+      data_source = (site.config['data_source'] || '_data')
       if !config
         return
       end
@@ -16,23 +27,27 @@ module Jekyll_Get
         config = [config]
       end
       config.each do |d|
-        begin
-          target = site.data[d['data']]
-          source = JSON.load(open(d['json']))
-          if target
-            HashJoiner.deep_merge target, source
-          else
-            site.data[d['data']] = source
-          end
-          if d['cache']
-            data_source = (site.config['data_source'] || '_data')
-            path = "#{data_source}/#{d['data']}.json"
-            open(path, 'wb') do |file|
-              file << JSON.generate(site.data[d['data']])
+        if d['cache']
+          site.data[d['data']] = loadFromCache(data_source, d['data'])
+        end
+        if site.data[d['data']].nil?
+          begin
+            target = site.data[d['data']]
+            source = JSON.load(open(d['json']))
+            if target
+              HashJoiner.deep_merge target, source
+            else
+              site.data[d['data']] = source
             end
+            if d['cache']
+              path = "#{data_source}/#{d['data']}.json"
+              open(path, 'wb') do |file|
+                file << JSON.generate(site.data[d['data']])
+              end
+            end
+          rescue
+            next
           end
-        rescue
-          next
         end
       end
     end
