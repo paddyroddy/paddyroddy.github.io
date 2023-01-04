fe62b50255720527a1017b1654bc6b54aff4ec54
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-01-04 11:03:18 +0000

    Consistent with quotes

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-JEKYLL_ENV=development
 bundle install
 bundle update --bundler
 bundle lock --add-platform x86_64-linux
-bundle exec jekyll serve
+JEKYLL_ENV=development bundle exec jekyll serve
